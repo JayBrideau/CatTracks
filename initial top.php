f4b7e9490dbd6f80e505565acd09b6c9971034e4
JayBrideau <jessicabrideau987@gmail.com>
2016-04-02 11:05:07 -0400

    create initial top.php
    created a simple top document to give website a title and some metadata
    will include css later

---
+++ initial top.php
@@ -0,0 +1,8 @@
+<head>
+    <title>CatTracks</title>
+    <meta name="description"
+              content="CatTracks is the student-oriented tool for undertanding your UVM CATS report."/>
+    <meta name="author"
+              content="Team 10" />
+    <meta charset="UTF-8" />
+</head>
